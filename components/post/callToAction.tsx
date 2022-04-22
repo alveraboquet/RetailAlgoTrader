871c177bdaa714bd2864015231fb1507c3a8c53b
drattylight <mlaux6@outlook.com>
2022-04-22 14:14:36 -0500

    Code and test call to action component

---
+++ components/post/callToAction.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+
+const CallToAction = () => {
+  return (
+    <div className="mt-md-4 mb-md-3 d-md-flex justify-content-center">
+      <aside className="bg-warning text-center rounded text-dark pt-3 pb-3 col-md-6 mb-3">
+        <p className="fs-5">
+          Start learning how algorithmic trading can increase your profitability
+          and efficiency for free
+        </p>
+        <button className="btn btn-dark">
+          <Link href="/signup">
+            <a className="text-decoration-none text-light">
+              Get started for free today
+            </a>
+          </Link>
+        </button>
+      </aside>
+    </div>
+  );
+};
+
+export default CallToAction;
