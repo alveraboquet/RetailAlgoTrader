bb8d611a2205de47dce2ecad97d0b856d14c279d
drattylight <mlaux6@outlook.com>
2022-05-04 15:20:34 -0500

    Add sessionProvider to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,20 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import 'bootstrap/dist/css/bootstrap.css';
 import { useEffect } from 'react';
+import { SessionProvider } from 'next-auth/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
   useEffect(() => {
     typeof document !== undefined
       ? require('bootstrap/dist/js/bootstrap')
       : null;
   }, []);
 
-  return <Component {...pageProps} />;
-}
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
 
-export default MyApp;
+export default App;
