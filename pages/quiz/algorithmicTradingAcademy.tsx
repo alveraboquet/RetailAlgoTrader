2608ac67fea0ee7ab16f9cba6c8ef469313b72c1
drattylight <mlaux6@outlook.com>
2022-05-04 15:21:22 -0500

    Replace header and footer with layout

--- pages/quiz/algorithmicTradingAcademy.tsx
+++ pages/quiz/algorithmicTradingAcademy.tsx
@@ -1,18 +1,17 @@
 import QuizLandingPage from '../../components/landingPages/quizLandingPage';
-import Header from '../../components/header/header';
-import Footer from '../../components/footer/footer';
+import Layout from '../../components/layout/layout';
 
 const TradingAcademyLandingPage = () => {
   return (
     <div className="text-center">
-      <Header></Header>
-      <QuizLandingPage
-        title="Algorithmic Trading Academy"
-        description="Algorithmic Trading Academy Description"
-        similar1Course="algorithmicTradingAcademy"
-        similar2Course="equitiesAndCryptoWithTradingview"
-      />
-      <Footer></Footer>
+      <Layout>
+        <QuizLandingPage
+          title="Algorithmic Trading Academy"
+          description="Algorithmic Trading Academy Description"
+          similar1Course="algorithmicTradingAcademy"
+          similar2Course="equitiesAndCryptoWithTradingview"
+        />
+      </Layout>
     </div>
   );
 };
