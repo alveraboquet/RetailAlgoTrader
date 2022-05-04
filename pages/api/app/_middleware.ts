ec31d1d5842f50101f89ed8b9952bbe2ebb2c668
drattylight <mlaux6@outlook.com>
2022-05-04 15:22:33 -0500

    Add middleware to api app routes

---
+++ pages/api/app/_middleware.ts
@@ -0,0 +1,21 @@
+export { default } from 'next-auth/middleware';
+
+import { NextRequest, NextResponse } from 'next/server';
+
+export const middleware = (req: NextRequest) => {
+  const origin = req.headers.get('origin');
+  const referrer = req.headers.get('referrer');
+  const xCustomHeader = req.headers.get('X-Custom-Header');
+
+  if (
+    xCustomHeader === 'lollipop' &&
+    (origin === 'http://localhost:3000' || // Will have to update these paths. Remove undefined if possible
+      origin === null ||
+      referrer === 'http://localhost:3000/' ||
+      referrer === null)
+  ) {
+    NextResponse.next();
+  } else {
+    return new Response(null, { status: 401 });
+  }
+};
