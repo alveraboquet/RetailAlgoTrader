9d77b402acdaa69641a1d9abce796ed46e020a4b
drattylight <mlaux6@outlook.com>
2022-05-05 15:19:55 -0500

    Add session check

--- pages/api/app/user/[id].ts
+++ pages/api/app/user/[id].ts
@@ -1,29 +1,37 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import pool from '../../../../db/index';
+import { getSession } from 'next-auth/react';
 
 const findUserById = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const {
-      query: { id },
-    } = req;
-    // Generate SQL statement
-    const statement = `SELECT first_name
-                         FROM member
-                         WHERE id = $1`;
-    const values = [id];
+  const session = await getSession({ req });
+  if (session) {
+    try {
+      const {
+        query: { id },
+      } = req;
+      // Generate SQL statement
+      const statement = `SELECT name
+                           FROM "User"
+                           WHERE id = $1`;
+      const values = [id];
 
-    // Execute SQL statement
-    const result = await pool.query(statement, values);
+      // Execute SQL statement
+      const result = await pool.query(statement, values);
 
-    if (result.rows?.length) {
-      res.status(200).json(result.rows[0]);
+      if (result.rows?.length) {
+        res.status(200).json(result.rows[0]);
+      }
+
+      return null;
+    } catch (err) {
+      if (typeof err === 'string') {
+        throw new Error(err);
+      }
     }
-
-    return null;
-  } catch (err) {
-    if (typeof err === 'string') {
-      throw new Error(err);
-    }
+  } else {
+    res
+      .status(401)
+      .send('You must be signed-in to view the protected content on this page');
   }
 };
 
--- pages/app/dashboard.tsx
+++ pages/app/dashboard.tsx
@@ -13,15 +13,17 @@ const Dashboard = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch('/api/app/user/1', {
+        const res = await fetch('/api/app/user/cl2tdnnv30071db0ayrgj9ts0', {
           headers: {
             'X-Custom-Header': 'lollipop',
           },
         });
         const data = await res.json();
-        if (data) {
-          setData(data.first_name);
+        if (data.name) {
+          setData(data.name);
+          return;
         }
+        setData('Failed to load data');
       } catch (err) {
         setData('Failed to load data');
       }
@@ -29,10 +31,12 @@ const Dashboard = () => {
     fetchData();
   }, [session]);
 
+  /*
   // If no session exists, display access denied message
   if (!session) {
     return <AccessDenied />;
   }
+  */
 
   // If session exists, display content
   return (
@@ -47,9 +51,18 @@ export default Dashboard;
 
 // Export the `session` prop to use sessions with Server Side Rendering
 export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
   return {
-    props: {
-      session: await getSession(context),
-    },
+    props: { session },
   };
 }
