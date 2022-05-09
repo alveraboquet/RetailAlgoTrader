a6679017dd97a86d7b26de0e982e80589c292e8b
drattylight <mlaux6@outlook.com>
2022-05-09 11:14:39 -0500

    Add initial data to landingpages

--- pages/quiz/equitiesAndCryptoWithTradingView.tsx
+++ pages/quiz/equitiesAndCryptoWithTradingView.tsx
@@ -1,9 +1,24 @@
-const EquitiesAndCryptoWithTradingview = () => {
+import QuizLandingPage from '../../components/landingPages/quizLandingPage';
+import Layout from '../../components/layout/layout';
+import coursesData from '../../components/landingPages/coursesData';
+
+const EquitiesAndCryptoWithTradingviewLandingPage = () => {
   return (
-    <div>
-      <p>Equities and Crypto with TradingView</p>
+    <div className="text-center">
+      <Layout>
+        <QuizLandingPage
+          title={coursesData.equitiesAndCryptoWithTradingview.title}
+          description={
+            coursesData.equitiesAndCryptoWithTradingview.longDescription
+          }
+          imagePath={coursesData.equitiesAndCryptoWithTradingview.imagePath}
+          curriculum={coursesData.equitiesAndCryptoWithTradingview.curriculum}
+          similar1Course="tradingAcademy"
+          similar2Course="forexTradingWithMT4"
+        />
+      </Layout>
     </div>
   );
 };
 
-export default EquitiesAndCryptoWithTradingview;
+export default EquitiesAndCryptoWithTradingviewLandingPage;
