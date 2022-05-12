6561ae326dc44d3e3731fc003d00d64eaaa89dad
drattylight <mlaux6@outlook.com>
2022-05-12 11:53:16 -0500

    Add API routes

---
+++ pages/api/app/chapters/completedChapters.ts
@@ -0,0 +1,42 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import pool from '../../../../db/index';
+import { getSession } from 'next-auth/react';
+
+const findCompletedChaptersByUser = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const session = await getSession({ req });
+  if (session) {
+    try {
+      const { userId } = session;
+      // Generate SQL statement
+      const statement = `SELECT "User_Chapter".chapter_id, "User_Chapter".completed, "Course_Chapter".course_id
+                           FROM "User_Chapter"
+                           INNER JOIN "Course_Chapter"
+                           ON "User_Chapter".chapter_id = "Course_Chapter".chapter_id
+                           WHERE "User_Chapter".user_id = $1
+                           ORDER BY "Course_Chapter".course_id DESC`;
+      const values = [userId];
+
+      // Execute SQL statement
+      const result = await pool.query(statement, values);
+
+      if (result.rows?.length) {
+        res.status(200).json(result.rows);
+      }
+
+      return null;
+    } catch (err) {
+      if (typeof err === 'string') {
+        throw new Error(err);
+      }
+    }
+  } else {
+    res
+      .status(401)
+      .send('You must be signed-in to view the protected content on this page');
+  }
+};
+
+export default findCompletedChaptersByUser;
---
+++ pages/api/app/courses/enrolledCourses.ts
@@ -0,0 +1,39 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import pool from '../../../../db/index';
+import { getSession } from 'next-auth/react';
+
+const findEnrolledCoursesByUser = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const session = await getSession({ req });
+  if (session) {
+    try {
+      const { userId } = session;
+      // Generate SQL statement
+      const statement = `SELECT course_id, enrolled
+                           FROM "User_Course"
+                           WHERE user_id = $1`;
+      const values = [userId];
+
+      // Execute SQL statement
+      const result = await pool.query(statement, values);
+
+      if (result.rows?.length) {
+        res.status(200).json(result.rows);
+      }
+
+      return null;
+    } catch (err) {
+      if (typeof err === 'string') {
+        throw new Error(err);
+      }
+    }
+  } else {
+    res
+      .status(401)
+      .send('You must be signed-in to view the protected content on this page');
+  }
+};
+
+export default findEnrolledCoursesByUser;
