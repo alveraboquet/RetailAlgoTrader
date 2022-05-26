db39c98734a10383ff89473125d167b8faa392f5
drattylight <mlaux6@outlook.com>
2022-05-26 13:46:49 -0500

    Add support for MDX

--- next.config.js
+++ next.config.js
@@ -84,3 +84,18 @@ module.exports = {
     ];
   },
 };
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+});
+module.exports = withMDX({
+  // Append the default value with md extensions
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+});
