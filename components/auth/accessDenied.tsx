268e27a19779fc63683f17885cb6eb897b2a640a
drattylight <mlaux6@outlook.com>
2022-05-26 13:47:15 -0500

    Refactor access denied component

--- components/auth/accessDenied.tsx
+++ components/auth/accessDenied.tsx
@@ -1,21 +1,25 @@
 import { signIn } from 'next-auth/react';
 import Layout from '../layout/layout';
+import Link from 'next/link';
 
 const AccessDenied = () => {
   return (
     <Layout>
-      <h1>Access Denied</h1>
-      <p>
-        <a
-          href="/api/auth/signin"
-          onClick={(e) => {
-            e.preventDefault();
-            signIn();
-          }}
-        >
-          You must be signed in to view this page
-        </a>
-      </p>
+      <div className="text-center">
+        <h1>Access Denied</h1>
+        <p>
+          <Link href="/api/auth/signin">
+            <a
+              onClick={(e) => {
+                e.preventDefault();
+                signIn();
+              }}
+            >
+              You must be signed in to view this page
+            </a>
+          </Link>
+        </p>
+      </div>
     </Layout>
   );
 };
