71fdc8e8d63e4988f1362b62391fd86e9bd06da4
drattylight <mlaux6@outlook.com>
2022-05-31 15:20:02 -0500

    Add supporting functions for stripe

---
+++ stripe.config.ts
@@ -0,0 +1,4 @@
+export const CURRENCY = 'usd';
+// Set your amount limits: Use float for decimal currencies and
+// Integer for zero-decimal currencies: https://stripe.com/docs/currencies#zero-decimal.
+export const EXPECTED_AMOUNT = 30.0;
