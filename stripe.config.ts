da2e1361f42a0915a3da153eac3c71815c6f0918
drattylight <mlaux6@outlook.com>
2022-06-01 17:41:03 -0500

    Add monthly and annual charges

--- stripe.config.ts
+++ stripe.config.ts
@@ -1,4 +1,5 @@
 export const CURRENCY = 'usd';
 // Set your amount limits: Use float for decimal currencies and
 // Integer for zero-decimal currencies: https://stripe.com/docs/currencies#zero-decimal.
-export const EXPECTED_AMOUNT = 30.0;
+export const MONTHLY_AMOUNT = 40.0;
+export const ANNUAL_AMOUNT = 30.0;
