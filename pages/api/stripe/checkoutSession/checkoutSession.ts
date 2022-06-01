f25f537c4541e69e8d8f0a630cfc8d066de4a994
drattylight <mlaux6@outlook.com>
2022-06-01 17:45:28 -0500

    Add check for monthly and annual charge amount

--- pages/api/stripe/checkoutSession/checkoutSession.ts
+++ pages/api/stripe/checkoutSession/checkoutSession.ts
@@ -1,5 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { CURRENCY, EXPECTED_AMOUNT } from '../../../../stripe.config';
+import {
+  CURRENCY,
+  MONTHLY_AMOUNT,
+  ANNUAL_AMOUNT,
+} from '../../../../stripe.config';
 import { formatAmountForStripe } from '../../../../lib/stripeHelpers';
 import Stripe from 'stripe';
 
@@ -21,28 +25,31 @@ export default async function handler(
     const amount: number = req.body.amount;
     try {
       // Validate the amount that was passed from the client.
-      if (!(amount === EXPECTED_AMOUNT)) {
-        throw new Error('Invalid amount.');
-      }
-      // Create Checkout Sessions from body params.
-      const params: Stripe.Checkout.SessionCreateParams = {
-        submit_type: 'pay',
-        payment_method_types: ['card'],
-        line_items: [
-          {
-            name: 'Pay RetailAlgoTrader',
-            amount: formatAmountForStripe(amount, CURRENCY),
-            currency: CURRENCY,
-            quantity: 1,
-          },
-        ],
-        success_url: `${req.headers.origin}/app/result?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${req.headers.origin}/app/proSignup`,
-      };
-      const checkoutSession: Stripe.Checkout.Session =
-        await stripe.checkout.sessions.create(params);
+      if (amount === MONTHLY_AMOUNT || amount === ANNUAL_AMOUNT) {
+        // Create Checkout Sessions from body params.
+        const params: Stripe.Checkout.SessionCreateParams = {
+          submit_type: 'pay',
+          payment_method_types: ['card'],
+          line_items: [
+            {
+              name: 'Pay RetailAlgoTrader',
+              amount: formatAmountForStripe(amount, CURRENCY),
+              currency: CURRENCY,
+              quantity: 1,
+            },
+          ],
+          success_url: `${req.headers.origin}/app/result?session_id={CHECKOUT_SESSION_ID}`,
+          cancel_url: `${req.headers.origin}/app/proSignup`,
+        };
+        const checkoutSession: Stripe.Checkout.Session =
+          await stripe.checkout.sessions.create(params);
 
-      res.status(200).json(checkoutSession);
+        res.status(200).json(checkoutSession);
+      } else {
+        res
+          .status(500)
+          .json({ statusCode: 500, message: 'Unable to Complete Request' });
+      }
     } catch (err) {
       const errorMessage =
         err instanceof Error ? err.message : 'Internal server error';
