b871510f98de2a0298d8005516789b9f7d9ebc1b
drattylight <mlaux6@outlook.com>
2022-06-02 16:24:18 -0500

    Change to subscription method

--- pages/api/stripe/checkoutSession/checkoutSession.ts
+++ pages/api/stripe/checkoutSession/checkoutSession.ts
@@ -1,11 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import {
-  CURRENCY,
-  MONTHLY_AMOUNT,
-  ANNUAL_AMOUNT,
-} from '../../../../stripe.config';
-import { formatAmountForStripe } from '../../../../lib/stripeHelpers';
+import { MONTHLY_AMOUNT, ANNUAL_AMOUNT } from '../../../../stripe.config';
 import Stripe from 'stripe';
+import { getSession } from 'next-auth/react';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   // https://github.com/stripe/stripe-node#configuration
@@ -21,42 +17,54 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === 'POST') {
-    const amount: number = req.body.amount;
-    try {
-      // Validate the amount that was passed from the client.
-      if (amount === MONTHLY_AMOUNT || amount === ANNUAL_AMOUNT) {
+  const session = await getSession({ req });
+
+  if (session) {
+    if (req.method === 'POST') {
+      const amount: number = req.body.amount;
+      try {
+        let subscriptionType;
+        // Validate the amount that was passed from the client.
+        if (amount === MONTHLY_AMOUNT)
+          subscriptionType = process.env.STRIPE_MONTHLY_SUBSCRIPTION;
+        else if (amount === ANNUAL_AMOUNT)
+          subscriptionType = process.env.STRIPE_ANNUAL_SUBSCRIPTION;
+        else throw new Error();
         // Create Checkout Sessions from body params.
         const params: Stripe.Checkout.SessionCreateParams = {
-          submit_type: 'pay',
+          mode: 'subscription',
+          customer: session.user.stripeCustomerId,
           payment_method_types: ['card'],
           line_items: [
             {
-              name: 'Pay RetailAlgoTrader',
-              amount: formatAmountForStripe(amount, CURRENCY),
-              currency: CURRENCY,
+              price: subscriptionType, // Product API Key from Stripe
               quantity: 1,
             },
           ],
           success_url: `${req.headers.origin}/app/result?session_id={CHECKOUT_SESSION_ID}`,
           cancel_url: `${req.headers.origin}/app/proSignup`,
+          subscription_data: {
+            metadata: {
+              // Adds note on Stripe so we can manually check if a user is Pro member
+              payingUserId: session.user.id,
+            },
+          },
         };
         const checkoutSession: Stripe.Checkout.Session =
           await stripe.checkout.sessions.create(params);
-
         res.status(200).json(checkoutSession);
-      } else {
-        res
-          .status(500)
-          .json({ statusCode: 500, message: 'Unable to Complete Request' });
+      } catch (err) {
+        const errorMessage =
+          err instanceof Error ? err.message : 'Internal server error';
+        res.status(500).json({ statusCode: 500, message: errorMessage });
       }
-    } catch (err) {
-      const errorMessage =
-        err instanceof Error ? err.message : 'Internal server error';
-      res.status(500).json({ statusCode: 500, message: errorMessage });
+    } else {
+      res.setHeader('Allow', 'POST');
+      res.status(405).end('Method Not Allowed');
     }
   } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
+    res
+      .status(401)
+      .send('You must be signed-in to view the protected content on this page');
   }
 }
