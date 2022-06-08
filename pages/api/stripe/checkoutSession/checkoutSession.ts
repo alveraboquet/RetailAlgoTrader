22b90878315daf97d7959bd09c122fb9a0b86c08
drattylight <mlaux6@outlook.com>
2022-06-08 17:27:05 -0500

    Remove comment

--- pages/api/stripe/checkoutSession/checkoutSession.ts
+++ pages/api/stripe/checkoutSession/checkoutSession.ts
@@ -4,7 +4,6 @@ import Stripe from 'stripe';
 import { getSession } from 'next-auth/react';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  // https://github.com/stripe/stripe-node#configuration
   apiVersion: '2020-08-27',
 });
 
