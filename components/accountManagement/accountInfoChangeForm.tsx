57dd0533b3eda18c1d563f95526787733d9c7457
drattylight <mlaux6@outlook.com>
2022-06-14 18:46:04 -0500

    Add form for changing account info

---
+++ components/accountManagement/accountInfoChangeForm.tsx
@@ -0,0 +1,112 @@
+import DOMPurify from 'isomorphic-dompurify';
+import React from 'react';
+
+type SetAccountSettingsChange = () => void;
+
+interface Props {
+  handleAccountInfoChange: (e: React.FormEvent) => void;
+  accountDetails: {
+    name: string;
+    email: string;
+  };
+  setAccountSettingsChangeTrue: SetAccountSettingsChange;
+  setAccountSettingsChangeFalse: SetAccountSettingsChange;
+  isAccountSettingsChanged: boolean;
+}
+
+/**
+ *
+ * @param param0 - functions and props from AccountManagement page
+ * @returns - form for changing account info in database
+ */
+const AccountInfoChangeForm = ({
+  handleAccountInfoChange,
+  accountDetails,
+  setAccountSettingsChangeTrue,
+  setAccountSettingsChangeFalse,
+  isAccountSettingsChanged,
+}: Props) => {
+  return (
+    <form
+      onSubmit={handleAccountInfoChange}
+      className="form rounded"
+      noValidate
+      id="accountSettings"
+    >
+      <div className="row">
+        <div className="col-3">
+          <label htmlFor="newName" className="form-label">
+            Name:
+          </label>
+        </div>
+        <div className="col-3">
+          <input
+            id="newName"
+            type="text"
+            autoComplete="given-name"
+            className="form-control"
+            placeholder={DOMPurify.sanitize(accountDetails.name)}
+            maxLength={20}
+            onChange={() => {
+              setAccountSettingsChangeTrue();
+            }}
+          />
+        </div>
+      </div>
+      <div className="row mt-4">
+        <div className="col-3">
+          <label htmlFor="newEmail" className="form-label">
+            Email:
+          </label>
+        </div>
+        <div className="col-3">
+          <input
+            id="newEmail"
+            type="email"
+            autoComplete="email"
+            aria-describedby="emailHelp"
+            className="form-control"
+            placeholder={DOMPurify.sanitize(accountDetails.email)}
+            maxLength={200}
+            onChange={() => {
+              setAccountSettingsChangeTrue();
+            }}
+          />
+          <div className="invalid-feedback">
+            Please ensure email is of form johnsmith@email.com
+          </div>
+        </div>
+      </div>
+      <div id="accountAlertPlaceholder"></div>
+      <div className="text-end">
+        {isAccountSettingsChanged ? (
+          <button
+            type="button"
+            className="btn btn-light border me-3"
+            data-testid="cancelAccountInfoChangeButton"
+            onClick={() => {
+              setAccountSettingsChangeFalse();
+              (
+                document.getElementById('accountSettings') as HTMLFormElement
+              ).reset();
+            }}
+          >
+            Cancel
+          </button>
+        ) : (
+          <></>
+        )}
+
+        <button
+          type="submit"
+          className="btn btn-warning"
+          disabled={!isAccountSettingsChanged}
+        >
+          Save
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default AccountInfoChangeForm;
