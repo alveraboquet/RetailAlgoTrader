4b7f46b3f2172015ddce888932e0e05eb2541359
drattylight <mlaux6@outlook.com>
2022-06-15 09:07:48 -0500

    Add test for createCustomerPortalSession form

---
+++ __tests__/components/accountManagement/createCustomerPortalSessionForm.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+import CreateCustomerPortalSessionForm from '../../../components/accountManagement/createCustomerPortalSessionForm';
+import { SessionProvider } from 'next-auth/react';
+
+describe('<CreateCustomerPortalSessionForm />', () => {
+  test('renders manage billing button', () => {
+    render(
+      <SessionProvider
+        session={{
+          expires: '1',
+          user: {
+            id: 'testId',
+            email: 'testEmail@email.com',
+            name: 'testUser',
+            stripeCustomerId: 'testStripeid',
+            isPro: false,
+          },
+        }}
+      >
+        <CreateCustomerPortalSessionForm />
+      </SessionProvider>
+    );
+
+    const manageBillingButton = screen.getByRole('button', {
+      name: /manage billing/i,
+    });
+
+    expect(manageBillingButton).toBeInTheDocument();
+  });
+});
