a6183d2627e33de100a6795cc92e55cabe397997
drattylight <mlaux6@outlook.com>
2022-06-16 16:10:39 -0500

    Add course id to coursesData

--- components/landingPages/coursesData.ts
+++ components/landingPages/coursesData.ts
@@ -1,5 +1,6 @@
 interface CourseData {
   title: string;
+  id: number;
   shortDescription: string;
   longDescription: string;
   imagePath: string;
@@ -17,6 +18,7 @@ interface Courses {
 const coursesData: Courses = {
   algorithmicTradingAcademy: {
     title: 'Algorithmic Trading Academy',
+    id: 2,
     shortDescription:
       'If you are new to alogrithmic trading then this is the course for you. This covers algorithmic trading techniques that can be applied to a variety of markets.',
     longDescription: 'long',
@@ -28,6 +30,7 @@ const coursesData: Courses = {
   },
   equitiesAndCryptoWithTradingview: {
     title: 'Equities and Crypto with TradingView',
+    id: 3,
     shortDescription:
       'If you have some experience with trading but want to advance your skills try this resourse. This covers basic algorithmic trading with Pinescript on Tradingview.',
     longDescription: 'long',
@@ -39,6 +42,7 @@ const coursesData: Courses = {
   },
   forexTradingWithMT4: {
     title: 'Forex Trading with MT4',
+    id: 4,
     shortDescription:
       'If you do not have much trading capital or you cannot trade during the day this is the course for you. This course focuses on Forex which trades 24/7 and MT4 which is a free, easy-to-learn platform for writing trading algorithms.',
     longDescription: 'long',
@@ -50,6 +54,7 @@ const coursesData: Courses = {
   },
   futuresWithQuantconnect: {
     title: 'Futures with QuantConnect',
+    id: 5,
     shortDescription:
       'If you already have a decent grasp on the basics of trading then this course will be a good option for you. It focuses specifically on using QuantConnect, a premier algorithmic trading platform, with Futures. Futures provide margin which make them ideal options for maximizing profit if traded correctly.',
     longDescription: 'long',
@@ -61,6 +66,7 @@ const coursesData: Courses = {
   },
   marketResearchWithPython: {
     title: 'Market Research with Python',
+    id: 6,
     shortDescription:
       'The majority of traders focus on making many small trades. However, it is just as profitable, if not more profitable, to make less frequent, thorougly researched trades. Python with Jupyter notebooks and QuantConnect is the best option for analyzing data and market trends.',
     longDescription: 'long',
@@ -72,6 +78,7 @@ const coursesData: Courses = {
   },
   tradingAcademy: {
     title: 'Trading Academy',
+    id: 1,
     shortDescription:
       'If you have no or little experience with trading start here. This course covers the basics such as indicators, risk management, types of orders, etc. This resource will build a strong foundation for you trading career.',
     longDescription:
