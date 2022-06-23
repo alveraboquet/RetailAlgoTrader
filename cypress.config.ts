8344b2e1f84599f32f73273fbdd8b015bcae1ed2
drattylight <mlaux6@outlook.com>
2022-06-23 15:47:30 -0500

    Setup cypress social logins

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'cypress';
 
+import { plugins } from 'cypress-social-logins';
+
+const googleSocialLogin = plugins.GoogleSocialLogin;
+
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('task', {
+        GoogleSocialLogin: googleSocialLogin,
+      });
     },
+    baseUrl: 'http://localhost:3000',
+    // allow cypress to access NextAuth login provider pages
+    chromeWebSecurity: false,
   },
 });
