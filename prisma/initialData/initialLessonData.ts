486284ec815cc766706892ce28ef16c7abea59c9
drattylight <mlaux6@outlook.com>
2022-08-10 11:51:27 -0500

    Add initial data for prisma seed

--- prisma/initialData/initialLessonData.ts
+++ prisma/initialData/initialLessonData.ts
@@ -2,37 +2,37 @@ const initialLessonData = [
   {
     id: 1,
     name: 'intro',
-    chapter_id: 17,
+    chapter_id: 19,
   },
   {
     id: 2,
     name: 'scalingIntoAndOutOfTrades',
-    chapter_id: 17,
+    chapter_id: 19,
   },
   {
     id: 3,
     name: 'divergenceTrading',
-    chapter_id: 17,
+    chapter_id: 19,
   },
   {
     id: 4,
     name: 'multipleTimeframes',
-    chapter_id: 17,
+    chapter_id: 19,
   },
   {
     id: 5,
     name: 'atr',
-    chapter_id: 17,
+    chapter_id: 19,
   },
   {
     id: 6,
     name: 'leadingAssets',
-    chapter_id: 17,
+    chapter_id: 19,
   },
   {
     id: 7,
     name: 'cointegration',
-    chapter_id: 17,
+    chapter_id: 19,
   },
 ];
 
