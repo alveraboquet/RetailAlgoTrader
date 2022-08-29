046d42295ad26da5e6ed178cc67ed6629acf9944
drattylight <mlaux6@outlook.com>
2022-08-29 10:29:58 -0500

    Adjust pool import path

--- pages/api/app/user/changeAccountDetails.ts
+++ pages/api/app/user/changeAccountDetails.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { unstable_getServerSession } from 'next-auth';
-import pool from '../../../../db/index';
+import pool from '../../../../dbConfig';
 import {
   validateAlphaData,
   validateEmail,
