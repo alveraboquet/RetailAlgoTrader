5d03d8dc63b45f7ab512383143adcd08c4cc9bba
drattylight <mlaux6@outlook.com>
2022-08-30 19:12:33 -0500

    Add buttonText and buttonColor props to Stripe portal session form

--- components/accountManagement/createCustomerPortalSessionForm.tsx
+++ components/accountManagement/createCustomerPortalSessionForm.tsx
@@ -1,13 +1,21 @@
+interface Props {
+  buttonText: string;
+  buttonColor: string;
+}
+
 /**
  * Must use form to access API route as the browser will not allow a document follow on a redirect originated from a
  * fetch request
  * @returns - form to redirect to Stripe billing portal
  */
-const CreateCustomerPortalSessionForm = () => {
+const CreateCustomerPortalSessionForm = ({
+  buttonText,
+  buttonColor,
+}: Props) => {
   return (
     <form method="POST" action="/api/stripe/createCustomerPortalSession">
-      <button type="submit" className="btn btn-warning">
-        Manage Billing
+      <button type="submit" className={`btn ${buttonColor}`}>
+        {buttonText}
       </button>
     </form>
   );
