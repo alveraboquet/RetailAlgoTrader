fb3efdc51bb0b3d44a04a8c9ed6867676f9564b3
drattylight <mlaux6@outlook.com>
2022-08-31 16:45:06 -0500

    Change blog posts to flex

--- components/blog/blogContent.tsx
+++ components/blog/blogContent.tsx
@@ -2,73 +2,69 @@ import PostCard from '../../components/post/postCard';
 
 const BlogContent = () => {
   return (
-    <article className="container">
-      <div className="row">
-        <aside className="col-md-4 mb-3">
-          <PostCard
-            imagePath="/images/posts/whatAreMarketMakers/MarketMaker-Cover.webp"
-            imageHeight={1000}
-            imageWidth={1500}
-            imageAlt=""
-            cardTitle="Essential Facts of Market Makers in Today's Markets"
-            cardText="There is a lot of confusion and inaccurate beliefs around market makers
+    <article className="container d-flex flex-wrap mb-3">
+      <aside className="col-md-6 col-lg-4 mb-3">
+        <PostCard
+          imagePath="/images/posts/whatAreMarketMakers/MarketMaker-Cover.webp"
+          imageHeight={1000}
+          imageWidth={1500}
+          imageAlt=""
+          cardTitle="Essential Facts of Market Makers in Today's Markets"
+          cardText="There is a lot of confusion and inaccurate beliefs around market makers
         and their roles in the market."
-            postPath="/posts/whatAreMarketMakersAndHowDoTheyWork"
-            buttonText="Read More"
-          />
-        </aside>
-        <aside className="col-md-4 mb-3">
-          <PostCard
-            imagePath="/images/posts/howMuchMoney/growingMoney.webp"
-            imageHeight={1000}
-            imageWidth={1500}
-            imageAlt=""
-            cardTitle="How much money can I make trading?"
-            cardText="Many people imagine trading as an easy path to riches. Discover how much money you can reasonably make while trading."
-            postPath="/posts/howMuchMoneyCanIMakeTrading"
-            buttonText="Read More"
-          />
-        </aside>
-        <aside className="col-md-4 mb-3">
-          <PostCard
-            imagePath="/images/posts/mistakeAllNewTraders/path.webp"
-            imageHeight={1000}
-            imageWidth={1500}
-            imageAlt=""
-            cardTitle="The mistake all new retail traders should avoid"
-            cardText="Do not repeat one of the most common mistakes we see with novice retail traders."
-            postPath="/posts/mistakeAllNewRetailTradersShouldAvoid"
-            buttonText="Read More"
-          />
-        </aside>
-      </div>
-      <div className="row mb-5 ">
-        <aside className="col-md-4 mb-3">
-          <PostCard
-            imagePath="/images/posts/possibleToSucceedAsRetailTrader/banner.webp"
-            imageHeight={1000}
-            imageWidth={1500}
-            imageAlt=""
-            cardTitle="Find out if it is actually possible to succeed as a retail algorithmic trader"
-            cardText="Many people seem to believe that it is not possible to succeed long-term at trading. Find out why that is not accurate."
-            postPath="/posts/isItActuallyPossibleToSucceedAsARetailTrader"
-            buttonText="Read More"
-          />
-        </aside>
-        <aside className="col-md-4 mb-3">
-          <PostCard
-            imagePath="/images/posts/pinescriptGuide/pineScriptLogo.webp"
-            imageHeight={1000}
-            imageWidth={1500}
-            imageAlt=""
-            cardTitle="Pinescript Guide"
-            cardText="Learn how to develop your own indicators and strategies in Pinescript effortlessly. Algorithmic trading 
+          postPath="/posts/whatAreMarketMakersAndHowDoTheyWork"
+          buttonText="Read More"
+        />
+      </aside>
+      <aside className="col-md-6 col-lg-4 mb-3">
+        <PostCard
+          imagePath="/images/posts/howMuchMoney/growingMoney.webp"
+          imageHeight={1000}
+          imageWidth={1500}
+          imageAlt=""
+          cardTitle="How much money can I make trading?"
+          cardText="Many people imagine trading as an easy path to riches. Discover how much money you can reasonably make while trading."
+          postPath="/posts/howMuchMoneyCanIMakeTrading"
+          buttonText="Read More"
+        />
+      </aside>
+      <aside className="col-md-6 col-lg-4 mb-3">
+        <PostCard
+          imagePath="/images/posts/mistakeAllNewTraders/path.webp"
+          imageHeight={1000}
+          imageWidth={1500}
+          imageAlt=""
+          cardTitle="The mistake all new retail traders should avoid"
+          cardText="Do not repeat one of the most common mistakes we see with novice retail traders."
+          postPath="/posts/mistakeAllNewRetailTradersShouldAvoid"
+          buttonText="Read More"
+        />
+      </aside>
+      <aside className="col-md-6 col-lg-4 mb-3">
+        <PostCard
+          imagePath="/images/posts/possibleToSucceedAsRetailTrader/banner.webp"
+          imageHeight={1000}
+          imageWidth={1500}
+          imageAlt=""
+          cardTitle="Find out if it is actually possible to succeed as a retail algorithmic trader"
+          cardText="Many people seem to believe that it is not possible to succeed long-term at trading. Find out why that is not accurate."
+          postPath="/posts/isItActuallyPossibleToSucceedAsARetailTrader"
+          buttonText="Read More"
+        />
+      </aside>
+      <aside className="col-md-6 col-lg-4 mb-3">
+        <PostCard
+          imagePath="/images/posts/pinescriptGuide/pineScriptLogo.webp"
+          imageHeight={1000}
+          imageWidth={1500}
+          imageAlt=""
+          cardTitle="Pinescript Guide"
+          cardText="Learn how to develop your own indicators and strategies in Pinescript effortlessly. Algorithmic trading 
                       does not have to be difficult. Get started today."
-            postPath="/posts/pinescriptGuide"
-            buttonText="Read More"
-          />
-        </aside>
-      </div>
+          postPath="/posts/pinescriptGuide"
+          buttonText="Read More"
+        />
+      </aside>
     </article>
   );
 };
