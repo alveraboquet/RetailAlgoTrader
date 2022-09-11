28a1221c1a8bfa197671d0f7b714920d60dfa348
drattylight <mlaux6@outlook.com>
2022-09-11 18:12:24 -0500

    Add separate footer for last lesson of course

--- components/footer/footerLesson.tsx
+++ components/footer/footerLesson.tsx
@@ -10,6 +10,9 @@ interface Props {
   currentLessonId: number;
 }
 
+// Array of lesson IDs that are the last lessons in a course
+const lastLessonIDs = [92];
+
 // Footer component for lesson pages
 const FooterLesson = ({
   course,
@@ -19,34 +22,65 @@ const FooterLesson = ({
   nextLesson,
   currentLessonId,
 }: Props) => {
-  return (
-    <footer className="bg-dark fixed-bottom" style={{ zIndex: '1' }}>
-      <div className="container text-end text-md-center pt-1">
-        <Link href={`/app/courses/${course}/${prevChapter}/${prevLesson}`}>
-          <button className="btn btn-warning me-0 me-md-3 mb-1 fs-5">
-            Prev
-          </button>
-        </Link>
-        <Link href={`/app/courses/${course}/${nextChapter}/${nextLesson}`}>
-          <button
-            className="btn btn-warning ms-3 mb-1 fs-5"
-            onClick={() =>
-              updateCurrentLesson({
-                course,
-                prevChapter,
-                nextChapter,
-                prevLesson,
-                nextLesson,
-                currentLessonId,
-              })
-            }
-          >
-            Next
-          </button>
-        </Link>
-      </div>
-    </footer>
-  );
+  if (lastLessonIDs.includes(currentLessonId)) {
+    return (
+      <footer className="bg-dark fixed-bottom" style={{ zIndex: '1' }}>
+        <div className="container text-end text-md-center pt-1">
+          <Link href={`/app/courses/${course}/${prevChapter}/${prevLesson}`}>
+            <button className="btn btn-warning me-0 me-md-3 mb-1 fs-5">
+              Prev
+            </button>
+          </Link>
+          <Link href={'/app/dashboard'}>
+            <button
+              className="btn btn-warning ms-3 mb-1 fs-5"
+              onClick={() =>
+                updateCurrentLesson({
+                  course,
+                  prevChapter,
+                  nextChapter,
+                  prevLesson,
+                  nextLesson,
+                  currentLessonId,
+                })
+              }
+            >
+              Return to dashboard
+            </button>
+          </Link>
+        </div>
+      </footer>
+    );
+  } else {
+    return (
+      <footer className="bg-dark fixed-bottom" style={{ zIndex: '1' }}>
+        <div className="container text-end text-md-center pt-1">
+          <Link href={`/app/courses/${course}/${prevChapter}/${prevLesson}`}>
+            <button className="btn btn-warning me-0 me-md-3 mb-1 fs-5">
+              Prev
+            </button>
+          </Link>
+          <Link href={`/app/courses/${course}/${nextChapter}/${nextLesson}`}>
+            <button
+              className="btn btn-warning ms-3 mb-1 fs-5"
+              onClick={() =>
+                updateCurrentLesson({
+                  course,
+                  prevChapter,
+                  nextChapter,
+                  prevLesson,
+                  nextLesson,
+                  currentLessonId,
+                })
+              }
+            >
+              Next
+            </button>
+          </Link>
+        </div>
+      </footer>
+    );
+  }
 };
 
 export default FooterLesson;
